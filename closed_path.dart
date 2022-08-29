3aa564182c05f1516f17db5b44513a126ae5d31d
Omar Eldanasoury <30899458+omar-danasoury@users.noreply.github.com>
2022-08-29 20:40:50 +0300

    Added main method

--- closed_path.dart
+++ closed_path.dart
@@ -12,3 +12,11 @@ class ClosedPath {
   }
 }
 
+
+void main(List<String> args) {
+  final path = ClosedPath()
+    ..moveTo(math.Point(0, 0))
+    ..lineTo(math.Point(2, 0))
+    ..lineTo(math.Point(2, 2))
+    ..lineTo(math.Point(0, 2));
+}
