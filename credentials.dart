19c2c0fa37b33c7c756b809adea100cf4a80cd01
Omar Eldanasoury <30899458+omar-danasoury@users.noreply.github.com>
2022-08-29 20:28:02 +0300

    added copyWith() method

--- credentials.dart
+++ credentials.dart
@@ -2,4 +2,14 @@ class Credentials {
   const Credentials({this.email = '', this.password = ''});
   final String email;
   final String password;
+  
+  Credentials copyWith({
+    String? email,
+    String? password,
+  }) {
+    return Credentials(
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
 }
